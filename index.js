dee68c6ba90215caf69cf17328af8952df1e684b
abuhurayrasyam <abuhurayrasyamofficial@gmail.com>
2025-05-16 00:15:00 +0600

    CRUD's delete functionality addded

--- index.js
+++ index.js
@@ -93,6 +93,14 @@ async function run() {
         res.send(result);
     })
 
+    // Delete a Coffee From Database
+    app.delete('/users/:id', async (req, res) => {
+        const id = req.params.id;
+        const query = {_id: new ObjectId(id)};
+        const result = await users.deleteOne(query);
+        res.send(result);
+    })
+
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
